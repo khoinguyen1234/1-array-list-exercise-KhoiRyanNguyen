5f2c8ba68b579f64e73da3324220879e29b5c5d2
Khoi Nguyen <johnthebaptistnguyen@gmail.com>
2019-04-24 09:07:49 -0700

    wad

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
@@ -15,6 +16,77 @@ namespace ArrayListSimpleEx
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private ArrayList message = new ArrayList();
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            message.Add("I");
+            message.Add("love");
+            message.Add("programming");
+            message.Add("so");
+            message.Add("much");
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            string x = "";
+            for (int i = 0; i < message.Count; i++)
+            {
+                x += message[i] + " ";
+            }
+            lblMessage.Text = x;
+
+
+
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            message.Reverse();
+            string x = "";
+            for (int i = 0; i < message.Count; i++)
+            {
+
+                x += message[i] + " ";
+            }
+            lblMessage.Text = x;
+
+
+
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            message.Insert(1, txtSecondPos.Text);
+            string x = string.Empty;
+
+            for (int i = 0; i < message.Count; i++)
+            {
+
+                x += message[i] + " ";
+
+            }
+
+            lblMessage.Text = x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string x = string.Empty;
+
+            for (int i = 0; i < message.Count; i++)
+            {
+                x += message[i] + " ";
+            }
+            message.RemoveAt(1);
+            lblMessage.Text = x;
         }
     }
 }
